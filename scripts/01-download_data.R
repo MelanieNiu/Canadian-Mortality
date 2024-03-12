aefab2a80349cabbced9cd5ba43aa2ac8033ddc8
Yuchao Niu <yc.niu@utoronto.ca>
2024-03-12 18:02:35 -0400

    Updated data downloading and cleaning scripts

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,23 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Statistics Canada
+# Author: Yuchao Niu
+# Date: 11 March, 2024
+# Contact: yc.niu@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: N.A.
+# Any other information needed? N.A.
 
 #### Workspace setup ####
-library(opendatatoronto)
+
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+raw_data <- 
+  read.csv(
+    file = "https://www150.statcan.gc.ca/t1/tbl1/en/dtl!downloadDbLoadingData-nonTraduit.action?pid=1310080101&latestN=5&startDate=&endDate=&csvLocale=en&selectedMembers=%5B%5B%5D%2C%5B1%5D%2C%5B%5D%2C%5B1%2C2%5D%5D&checkedLevels=0D1%2C0D2%2C2D1%2C2D2")
 
 
-
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+#### Save data from the Statistics Canada website  ####
+write_csv(raw_data, "data/raw_data.csv") 
 
          
