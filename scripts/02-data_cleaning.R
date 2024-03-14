ba94e4fd70e75fc013fbdaa854f6a6afdf1a0995
Yuchao Niu <yc.niu@utoronto.ca>
2024-03-14 16:22:47 -0400

    Update data cleaning script

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -18,60 +18,26 @@ raw_data <- read.csv("inputs/data/raw_data.csv")
 col_name <- names(raw_data)
 raw_data <- raw_data %>%
   rename("Year" = "REF_DATE",
-         "Leading_Cause_Death" = "Leading.causes.of.death..ICD.10.",
+         "Cause" = "Leading.causes.of.death..ICD.10.",
          "Value" = "VALUE") %>%
-  select("Year", "Characteristics", "Leading_Cause_Death", "Value")
-# 
-# # Subset for Number of Deaths
-# number_data <- filter(raw_data, Characteristics == "Number of deaths") %>%
-#   select(Year, Leading_Cause_Death, Value) %>%
-#   rename(Number_of_Death = Value) %>%
-#   distinct(Year, Leading_Cause_Death, .keep_all = TRUE)
-# view(number_data)
-# 
-# # Subset for Ranking
-# ranking_data <- filter(raw_data, Characteristics == "Rank of leading causes of death") %>%
-#   select(Year, Leading_Cause_Death, Value) %>%
-#   rename(Ranking = Value)
-# 
-# 
-# # Identify causes of death with missing number of deaths or ranking
-# missing_number <- setdiff(unique(raw_data$Leading_Cause_Death), unique(number_data$Leading_Cause_Death))
-# missing_ranking <- setdiff(unique(raw_data$Leading_Cause_Death), unique(ranking_data$Leading_Cause_Death))
-# 
-# # Combine missing information
-# missing_info <- union(missing_number, missing_ranking)
-# 
-# print(missing_info)
-# 
-# # Create a full list of unique combinations of Year and Leading_Cause_Death
-# all_combinations <- expand.grid(Year = unique(raw_data$Year), Leading_Cause_Death = unique(raw_data$Leading_Cause_Death))
-
-# Prepare datasets for "Number of deaths" and "Rank of leading causes of death"
+  select("Year", "Characteristics", "Cause", "Value") %>%
+  mutate(Cause = sub("\\[.*", "", Cause))
+  
 number_of_deaths <- raw_data %>%
   filter(Characteristics == "Number of deaths") %>%
-  select(Year, Leading_Cause_Death, Number_of_Death = Value)
+  select(Year, Cause, Death = Value)
 
 ranking_of_death <- raw_data %>%
   filter(Characteristics == "Rank of leading causes of death") %>%
-  select(Year, Leading_Cause_Death, Ranking = Value)
+  select(Year, Cause, Ranking = Value)
 
 # Combine the subsets 
-combined_data <- full_join(number_of_deaths, ranking_of_death, by = c("Year", "Leading_Cause_Death"))
+combined_data <- full_join(number_of_deaths, ranking_of_death, by = c("Year", "Cause"))
 view(combined_data)
-
-# # Identify missing data for each characteristic by checking against all possible combinations
-# missing_number <- anti_join(all_combinations, number_of_deaths, by = c("Year", "Leading_Cause_Death"))
-# missing_ranking <- anti_join(all_combinations, ranking_of_death, by = c("Year", "Leading_Cause_Death"))
-# 
-# # View results
-# print(paste("Missing Number of Deaths: ", nrow(missing_number)))
-# print(paste("Missing Rankings: ", nrow(missing_ranking)))
-# 
 
 
 # Filter out rows containing yearly totals
-cleaned_data <- filter(combined_data, !grepl("\\[A00-Y89\\]", Leading_Cause_Death))
+cleaned_data <- filter(combined_data, !grepl("\\[A00-Y89\\]", Cause))
 view(cleaned_data)
 
 #### Save data ####
