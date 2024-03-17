e3a118ed50991550fa19aad8fadef50f240dd3f1
Yuchao Niu <yc.niu@utoronto.ca>
2024-03-17 05:25:27 +0000

    Updated simulation and test

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,38 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates dataset 
+# Author: Yuchao Niu
+# Date: 16 March 2024
+# Contact: yc.niu@utornoto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: N.A.
+# Any other information needed? N.A.
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(123) # Ensure reproducibility
+
+# Create a dataset
+num_observations <- 120
+years <- sample(2001:2023, num_observations, replace=TRUE)
+causes_of_death <- c('heart disease', 'kidney disease', 'cancer', 'lung disease', 'neuro', 'skin', 'skeletomuscular')
+death_causes <- sample(causes_of_death, num_observations, replace=TRUE)
+number_of_deaths <- sample(100:1000, num_observations, replace=TRUE)
+
+sim_data <- data.frame(Year=years, 'Cause of Death'=death_causes, 'Number of Deaths'=number_of_deaths)
+
+# Calculate the total number of deaths by cause
+death_counts_by_cause <- aggregate(dataset$`Number of Deaths`, by=list(Cause=dataset$`Cause of Death`), FUN=sum)
+names(death_counts_by_cause) <- c('Cause of Death', 'Total Deaths')
+
+# Rank the causes by the number of deaths
+death_counts_by_cause$Rank <- rank(-death_counts_by_cause$`Total Deaths`, ties.method='min')
+
+# Merge the ranking back to the original dataset
+dataset_with_rank <- merge(dataset, death_counts_by_cause[,c('Cause of Death', 'Rank')], by='Cause of Death')
 
 
 
--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,84 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests Mortality Dataset
+# Author: Yc.niu@utoro
+# Contact: yc.niu@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: N.A.
+# Any other information needed? N.A.
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(arrow)
 
-#### Test data ####
+#### Test Data ####
+# read cleaned data #
+cleaned_data <- read_parquet("outputs/data/cleaned_data.parquet")
+
+# Test for Valid Years Range
+if(all(cleaned_data$Year >= 2001 & cleaned_data$Year <= 2023)) {
+  print("Test Passed: All years are within the 2001 to 2023 range")
+} else {
+  print("Test Failed: Some years are outside the 2001 to 2023 range")
+}
+
+# Test for Non-negative Number of Deaths
+if (all(cleaned_data$Death >= 0, na.rm = TRUE)) {
+  print("Test Passed: Number of Deaths is non-negative")
+} else {
+  print("Test Failed: There are negative values in Number of Deaths")
+}
+
+# Test for No Missing Values
+if(sum(is.na(cleaned_data)) == 0) {
+  print("Test Passed: No missing values in the dataset")
+} else {
+  print("Test Failed: There are missing values in the dataset")
+}
+
+# Test for Unique Ranks per year
+cleaned_data %>%
+  group_by(Year, Cause) %>%
+  summarise(is_unique_rank = n_distinct(Ranking) == n()) %>%
+  ungroup() %>%
+  filter(is_unique_rank == FALSE) -> non_unique_rank_cases
+
+if (nrow(non_unique_rank_cases) == 0) {
+  print("Test Passed: Ranks are unique for each cause of death per year")
+} else {
+  print("Test Failed: Ranks are not unique for each cause of death per year")
+}
+
+# Test for No Duplicate Observations
+if(nrow(cleaned_data) == nrow(unique(cleaned_data))) {
+  print("Test Passed: No duplicate observations")
+} else {
+  print("Test Failed: There are duplicate observations")
+}
+
+# Test for Correct Data Types
+
+## Test for 'Death' column being numeric
+if (class(cleaned_data$Death) == "numeric") {
+  message("Test Passed: 'Death' column is numeric.")
+} else {
+  message("Test Failed: 'Death' column is not numeric.")
+}
+
+## Test for 'Ranking' column being numeric
+if (class(cleaned_data$Ranking) == "integer") {
+  message("Test Passed: 'Ranking' column is integer.")
+} else {
+  message("Test Failed: 'Ranking' column is not integer.")
+}
+
+## Test for 'Cause' column being character
+if (class(cleaned_data$Cause) == "character") {
+  message("Test Passed: 'Cause' column is character.")
+} else {
+  message("Test Failed: 'Cause' column is not character.")
+}
+
+
+
+
+
